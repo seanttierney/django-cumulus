c1c44fd63abdccf1c091b2fa9228986d042637e3
Sean Tierney <seant@initierlc.com>
2017-03-15 20:46:21 +0000

    quick fix for django>=1.10

--- cumulus/management/commands/syncfiles.py
+++ cumulus/management/commands/syncfiles.py
@@ -5,7 +5,7 @@ import os
 import re
 
 from django.conf import settings
-from django.core.management.base import CommandError, NoArgsCommand
+from django.core.management.base import CommandError, BaseCommand
 
 
 from cumulus.authentication import Auth
@@ -13,35 +13,50 @@ from cumulus.settings import CUMULUS
 from cumulus.storage import get_headers, get_content_type, get_gzipped_contents
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = "Synchronizes project static *or* media files to cloud files."
-    option_list = NoArgsCommand.option_list + (
-        optparse.make_option("-i", "--include", action="append", default=[],
-                             dest="includes", metavar="PATTERN",
-                             help="Include file or directories matching this glob-style "
-                                  "pattern. Use multiple times to include more."),
-        optparse.make_option("-e", "--exclude", action="append", default=[],
-                             dest="excludes", metavar="PATTERN",
-                             help="Exclude files or directories matching this glob-style "
-                                  "pattern. Use multiple times to exclude more."),
-        optparse.make_option("-w", "--wipe",
-                             action="store_true", dest="wipe", default=False,
-                             help="Wipes out entire contents of container first."),
-        optparse.make_option("-t", "--test-run",
-                             action="store_true", dest="test_run", default=False,
-                             help="Performs a test run of the sync."),
-        optparse.make_option("-q", "--quiet",
-                             action="store_true", dest="test_run", default=False,
-                             help="Do not display any output."),
-        optparse.make_option("-c", "--container",
-                             dest="container", help="Override STATIC_CONTAINER."),
-        optparse.make_option("-s", "--static",
-                             action="store_true", dest="syncstatic", default=False,
-                             help="Sync static files located at settings.STATIC_ROOT path."),
-        optparse.make_option("-m", "--media",
-                             action="store_true", dest="syncmedia", default=False,
-                             help="Sync media files located at settings.MEDIA_ROOT path."),
-    )
+    
+    def add_arguments(self,parser):
+        parser.add_argument("-i", "--include", action="append", default=[],
+            dest="includes", metavar="PATTERN",
+            help="Include file or directories matching this glob-style "
+                "pattern. Use multiple times to include more."
+        )
+
+        parser.add_argument("-e", "--exclude", action="append", default=[],
+            dest="excludes", metavar="PATTERN",
+            help="Exclude files or directories matching this glob-style "
+                "pattern. Use multiple times to exclude more."
+        )
+
+        parser.add_argument("-w", "--wipe",
+            action="store_true", dest="wipe", default=False,
+            help="Wipes out entire contents of container first."
+        )
+
+        parser.add_argument("-t", "--test-run",
+            action="store_true", dest="test_run", default=False,
+            help="Performs a test run of the sync."
+        )
+
+        parser.add_argument("-q", "--quiet",
+            action="store_true", dest="test_run", default=False,
+            help="Do not display any output."
+        )
+
+        parser.add_argument("-c", "--container",
+            dest="container", help="Override STATIC_CONTAINER."
+        )
+
+        parser.add_argument("-s", "--static",
+            action="store_true", dest="syncstatic", default=False,
+            help="Sync static files located at settings.STATIC_ROOT path."
+        )
+
+        parser.add_argument("-m", "--media",
+            action="store_true", dest="syncmedia", default=False,
+            help="Sync media files located at settings.MEDIA_ROOT path."
+        )
 
     def set_options(self, options):
         """
@@ -97,7 +112,7 @@ class Command(NoArgsCommand):
         self.skip_count = 0
         self.delete_count = 0
 
-    def handle_noargs(self, *args, **options):
+    def handle(self, *args, **options):
         # setup
         self.set_options(options)
         self._connection = Auth()._get_connection()
